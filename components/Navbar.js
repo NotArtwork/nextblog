0df2563925ecf7cf98ec8ca6db1a8f9526773777
NotArtwork <AntArce3@gmail.com>
2022-10-17 16:59:14 -0400

    added NavBar and CSS

---
+++ components/Navbar.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+
+const Navbar = () => {
+    const { user, username } = { }
+
+    return(
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <Link href="/">
+                        <button>Feed</button>
+                    </Link>
+                </li>
+                { username && (
+                    <>
+                    <li className="push-left">
+                        <Link href="/admin">
+                            <button className="btn-blue"> Write Posts</button>
+                        </Link>                        
+                    </li>
+
+                    <li>
+                        <Link href={`/${username}`}>
+                            <img src={user?.photoURL} />
+                        </Link>
+                    </li> 
+                    </>
+                    
+                )}
+
+                { !username && (
+                    <>
+                        <Link href="/enter">
+                            <button>Log in</button>
+                        </Link>
+                    </>
+                )}
+            </ul>
+        </nav>
+    )
+}
