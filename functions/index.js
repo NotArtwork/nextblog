fcae6d5de3840153b9db92dd0994e6052e20eefc
NotArtwork <AntArce3@gmail.com>
2022-10-17 10:33:21 -0400

    updated emulators

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,30 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin')
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+if (process.env.NODE_ENV !== 'production') {
+    process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080'
+}
+
+exports.aggregatePosts = functions.firestore.document('posts/{postId').onCreate( async (snap, context) => {
+
+    const postData = snap.data();
+    
+    const userRef = db.doc(`users/${postData.uid}`);
+    const userData = (await userRef.get()).data();
+
+    const currentTotal = (userData && userData.totalPosts) || 0;
+
+    const totalPosts = currentTotal + 1;
+
+    return userRef.set({ totalPosts }, { merge: true})
+
+
+
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
